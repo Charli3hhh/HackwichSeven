7d9ec1372e42b1f9f2c877495e847a66cf2f0309
Charles Ombao <combao@hawaii.edu>
2020-03-05 10:15:06 -1000

    Completed Parts 1-8 Finish up Problem Set March 05, 2020

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var displayLabel: UILabel!
+    
+   
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //set displayLbel to no text
+        
+        self.displayLabel.text = ""
+        self.view.backgroundColor = UIColor.red
     }
 
-
+    @IBAction func ButtonPressed(_ sender: Any) {
+        let userInputText = TextField.text
+        self.displayLabel.text = userInputText
+    }
+    
 }
 
